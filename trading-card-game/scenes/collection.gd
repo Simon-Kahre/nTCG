9def3bec4c8dc21655a929e43a2dc393ec8137a7
kahre.simon@gmail.com <kahre.simon@gmail.com>
2026-04-10 16:59:19 +0200

    Update graphics for buttons in collection scene

--- trading-card-game/scenes/collection.gd
+++ trading-card-game/scenes/collection.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var allCards: Array[PackedScene]
+var pressColor: StyleBoxFlat = preload("res://assets/styleBox/collectionButton.tres")
+var emptyColor: StyleBoxEmpty = preload("res://assets/styleBox/collectionButtonEmpty.tres")
 
 func _ready() -> void:
 	allCards = get_parent().allCards
@@ -10,5 +12,12 @@ func _ready() -> void:
 		var sprite: Sprite2D = createdCard.get_child(0)
 		var button = Button.new()
 		button.set_button_icon(sprite.texture)
+		override_styleboxes(button)
+		button.add_theme_stylebox_override("pressed", pressColor)
 		self.get_child(0).add_child(button)
 		createdCard.queue_free()
+
+func override_styleboxes(button: Button):
+	button.add_theme_stylebox_override("pressed", pressColor)
+	button.add_theme_stylebox_override("hover", emptyColor)
+	button.add_theme_stylebox_override("normal", emptyColor)
