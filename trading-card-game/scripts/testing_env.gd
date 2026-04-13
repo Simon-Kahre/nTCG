748963012a09d5a31b17165675ff9bc85e27f2ca
kahre.simon@gmail.com <kahre.simon@gmail.com>
2026-04-13 16:25:30 +0200

    Add buttons to bottom of screen

--- trading-card-game/scripts/testing_env.gd
+++ trading-card-game/scripts/testing_env.gd
@@ -10,11 +10,19 @@ var collectionScene: PackedScene = preload("res://scenes/collection.tscn")
 var homeScene: PackedScene = preload("res://scenes/home.tscn")
 var packScene: PackedScene = preload("res://scenes/packs.tscn")
 
+@onready var bottomButtons: HBoxContainer = $HBoxContainer
+
 var currentScene: availableScenes
 
 var allCards: Array[PackedScene]
 
 func _ready() -> void:
+	var screenSize = get_viewport_rect()
+	print(screenSize.end)
+	
+	bottomButtons.scale = Vector2(screenSize.end[0]/bottomButtons.size.x,screenSize.end[0]/bottomButtons.size.x)
+	bottomButtons.position.y = screenSize.end[1]-(40*bottomButtons.scale.y)
+
 	var dir = DirAccess.open("res://assets/createdObjects/cards")
 	if dir:
 		dir.list_dir_begin()
@@ -40,8 +48,20 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		elif event.pressed and event.keycode == KEY_P and currentScene != availableScenes.PACK:
 			switch_scene(availableScenes.PACK)
 
+func call_scene_switch(newScene: String) -> void:
+	if newScene == "Collection" and currentScene != availableScenes.COLLECTION:
+		switch_scene(availableScenes.COLLECTION)
+	elif newScene == "Home" and currentScene != availableScenes.HOME:
+		switch_scene(availableScenes.HOME)
+	elif newScene == "Pack" and currentScene != availableScenes.PACK:
+		switch_scene(availableScenes.PACK)
+
 func switch_scene(scene: availableScenes):
-	get_child(1).queue_free()
+	for child in get_children():
+		if child is Node2D:
+			child.queue_free()
+			break
+	#get_child(1).queue_free()
 	if scene == availableScenes.HOME:
 			currentScene = availableScenes.HOME
 			self.add_child(homeScene.instantiate())
