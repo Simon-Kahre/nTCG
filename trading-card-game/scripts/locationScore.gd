bb6755763c47d0b8a96c760b80a46fbb7d4af4f8
Simon-Kahre <kahre.simon@gmail.com>
2026-06-13 18:01:22 +0200

    Update scoring logic
    Now sets score based on the 'atk' value for each card present.

--- trading-card-game/scripts/locationScore.gd
+++ trading-card-game/scripts/locationScore.gd
@@ -1,14 +1,26 @@
 extends VBoxContainer
 
+@export var opCards: GridContainer
+@export var playerCards: GridContainer
+
 var playerScore: int = 0
 var opScore: int = 1
 
-func update_score_display() -> void:
+func update_score() -> void:
+	playerScore = 0
+	for card in playerCards.get_children():
+		playerScore += card.atk
+	
+	opScore = 0
+	for card in opCards.get_children():
+		opScore += card.atk
+	
 	var opLabel: Label = self.find_child("OpponentScore")
 	var playerLabel: Label = self.find_child("PlayerScore")
 	
 	opLabel.text = str(opScore)
 	playerLabel.text = str(playerScore)
 
+
 func _ready() -> void:
-	update_score_display()
+	update_score()
