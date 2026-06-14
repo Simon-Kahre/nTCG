5a0eee48d2aae2f0f291fd149c4fbeb448d8d2cd
Simon-Kahre <kahre.simon@gmail.com>
2026-06-14 17:16:40 +0200

    Make cards in hand automatically be centered

--- trading-card-game/scenes/playerActions.gd
+++ trading-card-game/scenes/playerActions.gd
@@ -9,15 +9,13 @@ func _ready() -> void:
 	add_testing_cards()
 	
 	playerHand = self.find_child("PlayerHand")
-	playerHand.position = Vector2(get_viewport_rect().end.x/2, get_viewport_rect().end.y - 50)
-	print(playerHand.position)
+	playerHand.position = Vector2(get_viewport_rect().end.x/2, get_viewport_rect().end.y - 200)
 	
-	var offset = 0
+	var offset: int = -int((50.0 * (float(len(playerCards)) / 2.0)))
 	for card in playerCards:
-		print("woo")
 		playerHand.add_child(card)
 		card.position.x = offset
-		offset += 10
+		offset += 50
 
 func add_testing_cards():
 	for card in testingCards:
