c1ee8254e4247a4ca07e12a29f606ac383b9ec65
Simon-Kahre <kahre.simon@gmail.com>
2026-06-14 17:18:41 +0200

    Update code structure in playerActions.gd

--- trading-card-game/scenes/playerActions.gd
+++ trading-card-game/scenes/playerActions.gd
@@ -5,20 +5,25 @@ var playerCards: Array[BasicCard]
 @export var testingCards: Array[PackedScene]
 var playerHand: Node2D
 
+
 func _ready() -> void:
-	add_testing_cards()
-	
 	playerHand = self.find_child("PlayerHand")
 	playerHand.position = Vector2(get_viewport_rect().end.x/2, get_viewport_rect().end.y - 200)
 	
-	var offset: int = -int((50.0 * (float(len(playerCards)) / 2.0)))
-	for card in playerCards:
-		playerHand.add_child(card)
-		card.position.x = offset
-		offset += 50
+	add_testing_cards()
+	center_cards()
+
 
 func add_testing_cards():
 	for card in testingCards:
 		var tempCard = card.instantiate()
 		
 		playerCards.append(tempCard)
+		playerHand.add_child(tempCard)
+
+
+func center_cards():
+	var offset: int = -int((50.0 * (float(len(playerCards)) / 2.0)))
+	for card in playerCards:
+		card.position.x = offset
+		offset += 50
