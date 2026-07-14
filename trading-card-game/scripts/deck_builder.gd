447a53685c95035c7cb3b2f692672c84cb65664f
Simon-Kahre <kahre.simon@gmail.com>
2026-07-14 14:39:33 +0200

    Fix dynamic scaling in the deck building

--- trading-card-game/scripts/deck_builder.gd
+++ trading-card-game/scripts/deck_builder.gd
@@ -8,11 +8,13 @@ func _ready():
 	UI.position.x = 50
 	UI.find_child("GridContainer").add_theme_constant_override("h_separation", 50)
 	UI.scale = Vector2(screenSize.end[0]/(UI.find_child("GridContainer").size.x + 50), screenSize.end[0]/(UI.find_child("GridContainer").size.x + 50))
-	print(UI.find_child("GridContainer").size)
 	collectedCardsContainer = UI.find_child("ScrollContainer").get_child(0)
 	collectedCardsContainer.add_theme_constant_override("h_separation", 40)
 	UI.find_child("ScrollContainer").offset_transform_enabled = true
-	UI.find_child("ScrollContainer").custom_minimum_size.y = 265/(screenSize.end[1]/(UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12 + 265))
+	print((screenSize.end[1] - (UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12)*UI.scale.y)/UI.scale.y)
+	#print(screenSize.end[1] - ((UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12 + 210) * ((screenSize.end[0]/screenSize.end[1])/(1920/1080))))
+	UI.find_child("ScrollContainer").custom_minimum_size.y = (screenSize.end[1] - (UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12 + 6)*UI.scale.y)/UI.scale.y
+	#265/(screenSize.end[1]/(UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12 + 265))
 	update_scroll_container()
 
 func update_scroll_container():
