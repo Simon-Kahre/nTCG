efa705bfaba48029857e34c0b753895932ca5c60
Simon-Kahre <kahre.simon@gmail.com>
2026-07-27 13:46:29 +0200

    Start fixing home scene UI

--- trading-card-game/scripts/home.gd
+++ trading-card-game/scripts/home.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 	var screenSize = get_viewport_rect()
 	uiButtons.scale = Vector2(screenSize.end[0]/uiButtons.size.x*2/5, screenSize.end[0]/uiButtons.size.x*2/5)
 	uiButtons.position = Vector2(screenSize.end[0]/2-(uiButtons.size.x/2*uiButtons.scale.x), screenSize.end[1]/4)
+	#infoLabel.add_theme_font_size_override()
 	var deckBuilder = load("res://scenes/deckBuilder.tscn").instantiate()
 	deckBuilder.name = "DeckBuilder"
 	self.add_child(deckBuilder)
@@ -32,13 +33,13 @@ func challenge_player():
 	else:
 		print("You need a deck first")
 		opacity = 3
-	#get_tree().change_scene_to_packed(load("res://scenes/Combat.tscn"))
 
 func change_deck():
 	$VBoxContainer/Button.visible = false
 	$VBoxContainer/Button2.visible = false
 	$VBoxContainer/Button.disabled = true
 	$VBoxContainer/Button2.disabled = true
+	$VBoxContainer/Label.visible = false
 	if get_parent().find_child("HBoxContainer"):
 		get_parent().find_child("HBoxContainer").visible = false
 	for child in self.get_children():
@@ -46,21 +47,14 @@ func change_deck():
 			child.update_scroll_container()
 			child.visible = true
 
-	"""var deckBuilder = load("res://scenes/deckBuilder.tscn").instantiate()
-	deckBuilder.name = "DeckBuilder"
-	self.add_child(deckBuilder)"""
-
 func finish_deck():
 	$VBoxContainer/Button.visible = true
 	$VBoxContainer/Button2.visible = true
 	$VBoxContainer/Button.disabled = false
 	$VBoxContainer/Button2.disabled = false
+	$VBoxContainer/Label.visible = true
 	if get_parent().find_child("HBoxContainer"):
 		get_parent().find_child("HBoxContainer").visible = true
 	for child in self.get_children():
 		if child.name == "DeckBuilder":
 			child.visible = false
-
-	"""for child in self.get_children():
-		if child.name == "DeckBuilder":
-			child.queue_free()"""
