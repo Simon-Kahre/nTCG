1a57765edd5e0b82f936a64c65a1922eb1c77c49
Simon-Kahre <kahre.simon@gmail.com>
2026-08-03 20:20:28 +0200

    Update functionality and style for viewing cards

--- trading-card-game/scenes/battle_stage.gd
+++ trading-card-game/scenes/battle_stage.gd
@@ -8,6 +8,7 @@ var attackDeck
 var betCount: int
 
 var optionUI
+var deckBuilder
 
 func _ready() -> void:
 	optionUI = find_child("Card Count Picker")
@@ -19,11 +20,37 @@ func confirm_count():
 	betCount = int(option.get_item_text(option.get_selected_id()))
 	optionUI.visible = false
 	rpc_id(opponentId, "set_bet_count", betCount)
+	start_deck_building()
 
 @rpc("any_peer")
 func set_bet_count(count: int):
 	betCount = count
+	start_deck_building()
 
 @rpc("any_peer","call_local")
 func set_opponent_id(id: int):
 	opponentId = id
+
+func start_deck_building():
+	optionUI.queue_free()
+	deckBuilder = load("res://scenes/deckBuilder.tscn").instantiate()
+	deckBuilder.find_child("UI").get_child(2).connect("pressed", confirm_deck)
+	deckBuilder.find_child("UI").get_child(0).text = "Attack Deck"
+	for child: TextureRect in deckBuilder.find_child("UI").get_child(1).get_children():
+		child.queue_free()
+	for i in range(betCount/2):
+		var slot = TextureRect.new()
+		slot.texture = load("res://assets/sprites/icon5.svg")
+		deckBuilder.find_child("UI").get_child(1).add_child(slot)
+	self.add_child(deckBuilder)
+
+func confirm_deck():
+	for child: TextureRect in deckBuilder.find_child("UI").get_child(1).get_children():
+		if !child.get_script():
+			return
+	
+	if deckBuilder.find_child("UI").get_child(0).text == "Attack Deck":
+		deckBuilder.find_child("UI").get_child(0).text = "Support Deck"
+	elif deckBuilder.find_child("UI").get_child(0).text == "Support Deck":
+		deckBuilder.queue_free()
+	pass
--- trading-card-game/scripts/deck_builder.gd
+++ trading-card-game/scripts/deck_builder.gd
@@ -5,10 +5,10 @@ var collectedCardsContainer: GridContainer
 
 func _ready():
 	var screenSize = get_viewport_rect()
-	UI.position.x = 100
+	UI.position.x = 0
 	UI.position.y = 50
 	UI.find_child("GridContainer").add_theme_constant_override("h_separation", 50)
-	UI.scale = Vector2(screenSize.end[0]/(UI.size.x + 100), screenSize.end[0]/(UI.size.x + 100))
+	UI.scale = Vector2(screenSize.end[0]/(UI.size.x + 150), screenSize.end[0]/(720 + 150))
 	collectedCardsContainer = UI.find_child("ScrollContainer").get_child(0).get_child(1)
 	collectedCardsContainer.add_theme_constant_override("h_separation", 40)
 	UI.find_child("ScrollContainer").custom_minimum_size.y = (screenSize.end[1] - (UI.find_child("GridContainer").size.y + 12 + UI.find_child("Confirm").size.y + 12 + 6)*UI.scale.y)/UI.scale.y
@@ -17,18 +17,26 @@ func _ready():
 func update_scroll_container():
 	for i in range(len(Player.cards)):
 		if i >= collectedCardsContainer.get_child_count():
+			
 			var tempButton = Button.new()
 			var tempCard = Player.cards[i].instantiate()
 			
 			tempButton.icon = tempCard.get_child(0).texture
+			
 			tempButton.toggle_mode = true
-			tempButton.connect("toggled", card_clicked.bind(Player.cards[i], tempButton))
-			tempButton.add_theme_color_override("icon_pressed_color", Color(0.808, 0.161, 0.443))
-			tempButton.add_theme_color_override("icon_hover_pressed_color", Color(0.808, 0.161, 0.443))
-			tempButton.add_theme_stylebox_override("normal", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
-			tempButton.add_theme_stylebox_override("hover", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
-			tempButton.add_theme_stylebox_override("pressed", load("res://assets/styleBox/deckSelection.tres"))
-			tempButton.add_theme_stylebox_override("hover_pressed", load("res://assets/styleBox/deckSelection.tres"))
+			if UI.find_child("Gridcontainer"):
+				tempButton.connect("toggled", card_clicked.bind(Player.cards[i], tempButton))
+				tempButton.add_theme_color_override("icon_pressed_color", Color(0.808, 0.161, 0.443))
+				tempButton.add_theme_color_override("icon_hover_pressed_color", Color(0.808, 0.161, 0.443))
+				tempButton.add_theme_stylebox_override("normal", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
+				tempButton.add_theme_stylebox_override("hover", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
+				tempButton.add_theme_stylebox_override("pressed", load("res://assets/styleBox/deckSelection.tres"))
+				tempButton.add_theme_stylebox_override("hover_pressed", load("res://assets/styleBox/deckSelection.tres"))
+			else:
+				tempButton.add_theme_stylebox_override("normal", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
+				tempButton.add_theme_stylebox_override("hover", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
+				tempButton.add_theme_stylebox_override("pressed", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
+				tempButton.add_theme_stylebox_override("hover_pressed", load("res://assets/styleBox/deckSelectionNotSelected.tres"))
 			
 			collectedCardsContainer.add_child(tempButton)
 
@@ -51,12 +59,13 @@ func card_clicked(toggledOn: bool, cardScene: PackedScene, button: Button, conne
 			connectedSlot.set_script(null)
 
 func finished():
-	var finalDeck: Array[PackedScene]
-	
-	for card in UI.find_child("GridContainer").get_children():
-		if card is BasicCard:
-			var packedScene = load(card.scenePath)
-			finalDeck.append(packedScene)
-	
-	Player.currentDeck = finalDeck
+	if UI.find_child("Gridcontainer"):
+		var finalDeck: Array[PackedScene]
+		
+		for card in UI.find_child("GridContainer").get_children():
+			if card is BasicCard:
+				var packedScene = load(card.scenePath)
+				finalDeck.append(packedScene)
+		
+		Player.currentDeck = finalDeck
 	self.get_parent().finish_deck()
